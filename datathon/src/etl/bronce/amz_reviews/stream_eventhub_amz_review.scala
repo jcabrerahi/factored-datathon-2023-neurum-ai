873e5dbffebd5dea89e66b5119a9f02787c0a863
jcabrerahi@gmail.com <jcabrerahi@gmail.com>
2023-07-28 02:38:21 +0000

    feat: Add the bronce stream amz_reviews notebook with scala

--- datathon/src/etl/bronce/amz_reviews/stream_eventhub_amz_review.scala
+++ datathon/src/etl/bronce/amz_reviews/stream_eventhub_amz_review.scala
@@ -5,13 +5,15 @@ import org.apache.spark.sql.functions.{ explode, split }
 // To connect to an Event Hub, EntityPath is required as part of the connection string.
 // Here, we assume that the connection string from the Azure portal does not have the EntityPath part.
 
-val path_endpoint = "Endpoint=sb://factored-datathon.servicebus.windows.net/;SharedAccessKeyName=datathon_listener;SharedAccessKey=sJJnyi8GGTBAa55jY89kacoT6hXAzWx2B+AEhCPEKYE=;EntityPath=factored_datathon_amazon_review"
+// val path_endpoint = "Endpoint=sb://factored-datathon.servicebus.windows.net/;SharedAccessKeyName=datathon_listener;SharedAccessKey=sJJnyi8GGTBAa55jY89kacoT6hXAzWx2B+AEhCPEKYE=;EntityPath=factored_datathon_amazon_review"
+val path_endpoint = "Endpoint=sb://factored-datathon.servicebus.windows.net/;SharedAccessKeyName=datathon_group_4;SharedAccessKey=zkZkK6UnK6PpFAOGbgcBfnHUZsXPZpuwW+AEhEH24uc=;EntityPath=factored_datathon_amazon_reviews_4"
 
 val connectionString = ConnectionStringBuilder(path_endpoint)
   .build
 
 val eventHubsConf = EventHubsConf(connectionString)
-  .setStartingPosition(EventPosition.fromEndOfStream)
+  .setStartingPosition(EventPosition.fromStartOfStream)
+  .setConsumerGroup("neurum_ai")
 
 
 // COMMAND ----------
@@ -24,3 +26,17 @@ val df_eventhubs = spark.readStream
 // COMMAND ----------
 
 display(df_eventhubs)
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions._
+
+val df_eventhubs_decoded = df_eventhubs.withColumn("body", col("body").cast("string"))
+
+// COMMAND ----------
+
+display(df_eventhubs_decoded)
+
+// COMMAND ----------
+
+
