bbc9c3071e9c0fcd0cdb4fc0a1f79223dc73429c
jcabrerahi@gmail.com <jcabrerahi@gmail.com>
2023-07-28 02:38:01 +0000

    feat: Add the bronce batch amz_reviews

--- datathon/src/etl/bronce/amz_reviews/batch_amz_reviews.py
+++ datathon/src/etl/bronce/amz_reviews/batch_amz_reviews.py
@@ -4,7 +4,20 @@
 
 # COMMAND ----------
 
-from datathon.src.utils.data_transformation import 
+!pip install unidecode boto3
+
+# COMMAND ----------
+
+from datathon.src.utils.data_transformation import DateTransformation
+from datathon.src.utils.data_transformation import EnrichingTransformation
+from datathon.config.logging import setup_logging
+from datathon.config.integration_config import AWSConfig
+
+from pyspark.sql.functions import col
+import logging
+import boto3
+import time
+from typing import List
 
 # COMMAND ----------
 
@@ -47,92 +60,118 @@ spark.conf.set(f"fs.azure.sas.fixed.token.{storage_account_name}.dfs.core.window
 
 # COMMAND ----------
 
-# path_bronce_invetory_data = f"s3a://{list_s3_credentials[0]}:{list_s3_credentials[1]}@{path_bucket_rg}/bronce/amazon/inventory_data"
+@setup_logging("logs_batch_amz_review")
+def load_chunked_data_from_paths(container_name: str, paths_partitions: List[str], path_folder: str, storage_account_name: str):
+    """
+     Loads data from a list of routes into a cumulative Spark DataFrame.
+
+     Parameters:
+     - container_name (str): Name of the container in which the data is located.
+     - paths_partitions (list): List of paths where the data partitions are located.
+     - path_folder (str): Path to the folder that contains the data partitions.
+     - storage_account_name (str): Name of the Azure storage account where the data is located.
+
+     Returns:
+     - df_all_partitions (DataFrame): Spark DataFrame containing all data loaded from the provided paths.
+
+     The function iterates through the list of routes, loading the data for each route into a Spark DataFrame.
+     The data for all routes is accumulated in a single DataFrame. Every 5 partitions is saved
+     the DataFrame in delta format in the path indicated in 'path_bronce_amz_reviews'.
+     In addition, transformations are performed on the extracted data, such as changing the date format.
+
+    """
+
+    df_all_partitions = None
+    counter = 0
+
+    logging.info("Iniciando proceso de carga de datos")
+
+    # Recorrer los paths_partitions y cargar los datos en el DataFrame acumulativo
+    for path_info in paths_partitions:
+        path = f"{path_folder}/{path_info.name}"
+        counter += 1
+        print(path, counter)
+
+        # Extract
+        df_partition = spark.read.format("json").load(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path}")
+
+        # Transform
+        df_reviews = EnrichingTransformation.get_formatted_column_names(df_partition)
+        df_reviews = DateTransformation.unix_to_date(df_reviews, "unix_review_time")
+        df_reviews = EnrichingTransformation.add_file_source_column(df_reviews)
+        df_reviews = (DateTransformation.extract_year_month(df_reviews, "unix_review_time_date")
+                      .withColumnRenamed("unix_review_time_date_year_month", "year_month")
+                      )
+
+        if df_all_partitions is None:
+            df_all_partitions = df_reviews
+        else:
+            df_all_partitions = df_all_partitions.union(df_reviews)
+        
+        # # Load
+        # == Append to delta table every 100 partitions
+        if counter % 500 == 0:
+            print("="*10,"Saving chunk into delta")
+            logging.info(f"Saving chunk into delta")
+            counter = 0
+
+            start_time = time.time()
+
+            (df_all_partitions.coalesce(1)
+                .write.format("delta")
+                .partitionBy('year_month')
+                .option("overwriteSchema", "true")
+                .mode("append")
+                .save(path_bronce_amz_reviews)
+            )
+
+            df_all_partitions = None
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+
+            logging.info(f"Saved chunk into delta | Last_partition_processed: {path} | Elapsed time: {elapsed_time} seconds")
+            
+    return df_all_partitions
 
 # COMMAND ----------
 
-path = "amazon_reviews/partition_2/"
-df_partition_1_reviews = spark.read.format("json").load(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path}")
+# Configure AWS credentials to logging
+aws_config = AWSConfig(access_key=aws_access_key, secret_key=aws_secret_key)
+aws_config.setup_aws_credentials()
+
+path_reviews = "amazon_reviews/"
+paths = dbutils.fs.ls(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path_reviews}")
+load_chunked_data_from_paths(container_name, paths, path_reviews[:-1], storage_account_name)
 
 # COMMAND ----------
 
-display(df_partition_1_reviews)
+# df_test.explain(mode='cost')
 
 # COMMAND ----------
 
-path = "amazon_metadata/partition_1/"
-dbutils.fs.ls(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path}")
-
-# COMMAND ----------
-
-path = "amazon_metadata/partition_1/"
-df_partition_1_metadata = spark.read.format("json").load(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path}")
-display(df_partition_1_metadata)
+dbutils.notebook.exit("Done")
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC # Save
+# MAGIC # Data quality
 
 # COMMAND ----------
 
-# df_all_partitions.explain(mode='cost')
+# 55933 per partition
+df_test = spark.read.format("delta").load(path_bronce_amz_reviews)
+# print(df_test.count())
+display(df_test.groupBy("file_source").count())
 
 # COMMAND ----------
 
-df_all_partitions.coalesce(1).write.format("delta").option("overwriteSchema", "true").mode("overwrite").save(path_bronce_amz_reviews)
-
-
-# COMMAND ----------
-
-#df_partition_1_reviews.coalesce(1).write.format("delta").option("overwriteSchema", "true").mode("overwrite").save(path_bronce_amz_reviews)
+print(df_test.count())
 
 # COMMAND ----------
 
-df = df_partition_1_reviews
+display(df_test.filter(col("asin") == "1622234642"))
 
 # COMMAND ----------
 
-df.explain(mode='cost')
 
-# COMMAND ----------
-
-path = "amazon_reviews/"
-
-path_list = dbutils.fs.ls(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path}")
-print(len(path_list))
-
-path_list
-
-# COMMAND ----------
-
-dbutils.fs.ls(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path}")
-
-# COMMAND ----------
-
-# Lista de paths
-path_reviews = "amazon_reviews/"
-paths = dbutils.fs.ls(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path_reviews}")
-
-# DataFrame acumulativo
-df_all_partitions = None
-
-# Recorrer los paths y cargar los datos en el DataFrame acumulativo
-for path_info in paths[:80]:
-    path = f"amazon_reviews/{path_info.name}"
-    print(path)
-    df_partition = spark.read.format("json").load(f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/{path}")
-
-    if df_all_partitions is None:
-        df_all_partitions = df_partition
-    else:
-        df_all_partitions = df_all_partitions.union(df_partition)
-
-# COMMAND ----------
-
-display(df_all_partitions)
-
-# COMMAND ----------
-
-# Save as table
-# df_all_partitions.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable("test_db.amazon.reviews_sample")
